cdfaeee4d46b04bb022b2cd850b9961a5201a62d
M Wahhab Mirza - Vector Labs FZE LLC <144521746+MWahhabMirzaVectorLabsFZELLC@users.noreply.github.com>
2024-09-20 05:50:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-    origin: 'http://localhost:5173',
-}));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
